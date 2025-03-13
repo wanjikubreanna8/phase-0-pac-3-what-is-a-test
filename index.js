3124dc2d8aa412ec39b04aff446e401986f8dd26
breanna <breannawanjiku8@gmail.com>
2025-03-13 09:36:27 +0300

    fourthmessage

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+describe("Name", () => {
+    it('returns "Susan"', () => {
+      expect(name).toEqual("Susan");
+    });
+  });
+
+  describe("Height", () => {
+    it("is less than 40", () => {
+      expect(height).toBeLessThan(40);
+    });
+  });
+
+  describe("Message", () => {
+    it("gives the name and height", () => {
+      expect(message).toInclude(name);
+      expect(message).toInclude(height);
+    });
+  });
+console. log(message);
